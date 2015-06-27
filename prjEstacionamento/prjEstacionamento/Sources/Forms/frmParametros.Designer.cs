25301b18745781406b0073f38ee00e18a2f96f9c
mendesfelipe87 <mendes_felipe@live.com>
2015-06-27 09:33:32 -0300

    ++ parametros

--- prjEstacionamento/prjEstacionamento/Sources/Forms/frmParametros.Designer.cs
+++ prjEstacionamento/prjEstacionamento/Sources/Forms/frmParametros.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace prjEstacionamento
 {
-    partial class Form1
+    partial class frmParametros
     {
         /// <summary>
         /// Required designer variable.
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // frmParametros
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(938, 392);
+            this.Name = "frmParametros";
+            this.Text = "Parametros";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- prjEstacionamento/prjEstacionamento/Sources/Forms/frmParametros.cs
+++ prjEstacionamento/prjEstacionamento/Sources/Forms/frmParametros.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace prjEstacionamento
 {
-    public partial class Form1 : Form
+    public partial class frmParametros : Form
     {
-        public Form1()
+        public frmParametros()
         {
             InitializeComponent();
         }
