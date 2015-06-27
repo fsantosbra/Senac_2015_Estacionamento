7ee6cdd0eacf1d44acbbedd12a65f7e6ea2f50b7
mendesfelipe87 <mendes_felipe@live.com>
2015-06-27 09:47:27 -0300

    ++ parametros

---
+++ prjEstacionamento/prjEstacionamento/Sources/Class/Parametros.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prjEstacionamento.Sources.Class
+{
+    class Parametros
+    {
+
+        private string Nome { get; set; }
+        private string Valor { get; set; }
+
+
+
+
+    }
+}
---
+++ prjEstacionamento/prjEstacionamento/Sources/DAO/daoParametros.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prjEstacionamento.Sources.DAO
+{
+    class daoParametros
+    {
+    }
+}
