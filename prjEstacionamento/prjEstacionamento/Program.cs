acffec7f0a0e146c4613ad9adcfae2f2520d2ddf
edynunes <edsonpiresnunes@hotmail.com>
2015-06-27 11:05:14 -0300

    Teste 123123

--- prjEstacionamento/prjEstacionamento/Program.cs
+++ prjEstacionamento/prjEstacionamento/Program.cs
@@ -11,7 +11,6 @@ namespace prjEstacionamento
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
-        /// 
         [STAThread]
         static void Main()
         {
