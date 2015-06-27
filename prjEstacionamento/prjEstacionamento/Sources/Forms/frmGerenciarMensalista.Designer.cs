8ce28cc73213ba615633218a4d9216382f8cd731
Edy Nunes <edsonpiresnunes@hotmail.com>
2015-06-27 16:10:35 -0300

    Teste

--- prjEstacionamento/prjEstacionamento/Sources/Forms/frmGerenciarMensalista.Designer.cs
+++ prjEstacionamento/prjEstacionamento/Sources/Forms/frmGerenciarMensalista.Designer.cs
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // frmGerenciarMensalista
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(663, 389);
+            this.Name = "frmGerenciarMensalista";
             this.Text = "frmGerenciarMensalista";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
