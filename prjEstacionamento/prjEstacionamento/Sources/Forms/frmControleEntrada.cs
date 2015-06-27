e69e36bd328f827d32a8e1450e0d8585c5fd6ac6
BrunoAndradeSA <bruno.a.s.almeida@gmail.com>
2015-06-27 14:50:19 -0300

    Controle de Entrada

---
+++ prjEstacionamento/prjEstacionamento/Sources/Forms/frmControleEntrada.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prjEstacionamento.Sources.Forms
+{
+    public partial class frmControleEntrada : Form
+    {
+        public frmControleEntrada()
+        {
+            InitializeComponent();
+            txtDataEntrada.Text = DateTime.Now.ToString;
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+        }
+    }
+}
