c076db9d8f7b44430df14f6e080093034388354b
mendesfelipe87 <mendes_felipe@live.com>
2015-06-27 09:58:41 -0300

    ++ daoBase conexao bd

---
+++ prjEstacionamento/prjEstacionamento/Sources/DAO/daoBase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prjEstacionamento.Sources.DAO
+{
+    class daoBase
+    {
+
+        protected string connString { get; set; }
+        protected SqlConnection conexao { get; set; }
+        protected SqlCommand comando { get; set; }
+
+        public daoBase()
+        {
+            conexao = new SqlConnection();
+            comando = new SqlCommand();
+        }
+
+    }
+}
