9e0b0061e7490e7d360d07cd4606526d0540684f
Daikitis <daikitis@gmail.com>
2015-06-30 11:54:39 -0300

    Arquivos daovagas e classe vagas

---
+++ prjEstacionamento/prjEstacionamento/Sources/Class/Vagas.cs
@@ -0,0 +1,29 @@
+﻿using prjEstacionamento.Sources.DAO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prjEstacionamento.Sources.Class
+{
+    class Vagas
+    {
+        public int Id { get; set; }
+        public string TipoVeiculoId { get; set; }
+        public int QtdeVagasTotal { get; set; }
+        public string QtdeMensalista { get; set; }
+        public string QtdeVagasTotalCorrente { get; set; }
+        public string QtdeMensalistaCorrente { get; set; }
+        public daoVagas daoVagas { get; set; }
+        public Vagas()
+        {
+            this.daoVagas = new daoVagas();
+        }
+
+        public int InserirVaga(Vagas vagas)
+        {
+            return this.daoVagas.InserirVaga(vagas);
+        }
+    }
+}
---
+++ prjEstacionamento/prjEstacionamento/Sources/DAO/daoVagas.cs
@@ -0,0 +1,101 @@
+﻿using prjEstacionamento.Sources.Class;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prjEstacionamento.Sources.DAO
+{
+    class daoVagas : daoBase
+    {
+        private string Insert = @"INSERT INTO VAGAS VALUES (@TIPOVEICULOID, @QTDEVAGASTOTAL, @QTDEMENSALISTA, @QTDEVAGASTOTALCORRENTE, 
+                                                            @QTDEMENSALISTACORRENTE);
+                                  SELECT SCOPE_IDENTITY();";
+        private string Update = @"UPDATE VAGAS SET TipoVeiculoId = @TIPOVEICULOID
+                                                  ,QtdeVagasTotal = @QTDEVAGASTOTAL
+                                                  ,QtdeVagasMensalista = @QTDEMENSALISTA
+                                                  ,QtdeVagasTotalCorrente = @QTDEVAGASTOTALCORRENTE
+                                                  ,QtdeMensalistaCorrente = @QTDEMENSALISTACORRENTE
+                                  WHERE Id = @Id";
+        
+        private string Delete = @"DELETE FROM VAGAS WHERE Id = @Id";
+        
+        private string Select = @"SELECT ID
+                                        ,TIPOVEICULOID
+                                        ,QTDEVAGASTOTAL
+                                        ,QTDEMENSALISTA
+                                        ,QTDEVAGASTOTALCORRENTE
+                                        ,QTDEMENSALISTACORRENTE 
+                                  FROM VAGAS";
+        private string SelectSpecific = @"SELECT ID
+                                                ,TIPOVEICULOID
+                                                ,QTDEVAGASTOTAL
+                                                ,QTDEMENSALISTA
+                                                ,QTDEVAGASTOTALCORRENTE
+                                                ,QTDEMENSALISTACORRENTE  
+                                         FROM VAGAS WHERE ID =  LIKE '%'+ @LOGRADOURO +'%'";
+
+        public daoVagas()
+        {
+            base.conexao = new SqlConnection(base.connString);
+            base.comando = new SqlCommand();
+            base.comando.Connection = base.conexao;
+        }
+
+        public int InserirVaga(Vagas vagas)
+        {
+            var vagaId = 0;
+
+            try
+            {
+                var paramId = new SqlParameter("@ID", vagas.Id);
+                var paramTipoVeiculoId = new SqlParameter("@TIPOVEICULOID", vagas.TipoVeiculoId);
+                var paramQtdeVagasTotal = new SqlParameter("@QTDEVAGASTOTAL", vagas.QtdeVagasTotal);
+                var paramQtdeMensalista = new SqlParameter("@QTDEMENSALISTA", vagas.QtdeMensalista);
+                var paramQtdeVagasTotalCorrente = new SqlParameter("@QTDEVAGASTOTALCORRENTE", vagas.QtdeVagasTotalCorrente);
+                var paramQtdeMensalistaCorrente = new SqlParameter("@QTDEMENSALISTACORRENTE", vagas.QtdeMensalistaCorrente);
+
+                base.comando.CommandText = Insert;
+                base.comando.Parameters.Add(paramId);
+                base.comando.Parameters.Add(paramTipoVeiculoId);
+                base.comando.Parameters.Add(paramQtdeVagasTotal);
+                base.comando.Parameters.Add(paramQtdeMensalista);
+                base.comando.Parameters.Add(paramQtdeVagasTotalCorrente);
+                base.comando.Parameters.Add(paramQtdeMensalistaCorrente);
+
+                base.conexao.Open();
+                vagaId = Convert.ToInt32(base.comando.ExecuteScalar());
+            }
+            finally
+            {
+                base.conexao.Close();
+                base.comando.Parameters.Clear();
+            }
+
+            return vagaId;
+        }
+
+        public DataTable ListarVagas()
+        {
+            try
+            {
+                var tabelaVagas = new DataTable();
+
+                base.comando.CommandText = Select;
+                var dataReader = base.comando.ExecuteReader();
+
+                tabelaVagas.Load(dataReader);
+
+                return tabelaVagas;
+            }
+            finally
+            {
+                base.conexao.Close();
+                base.comando.Parameters.Clear();
+            }
+        }
+    }
+}
